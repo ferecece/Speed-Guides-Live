e1669cdbc49097719f0a5e3c18fd06fbd518803f
ferecece <142701046+ferecece@users.noreply.github.com>
2024-08-17 02:31:21 -0400

    Added compiled component

--- Package.bat
+++ Package.bat
@@ -2,7 +2,7 @@
 
 if [%1]==[] goto usage
 
-set OUT_DIR=.\official_builds\%1
+set OUT_DIR=.\unofficial_builds\%1
 mkdir %OUT_DIR%
 
 REM copy relevant files to proper directory
@@ -11,9 +11,18 @@ xcopy .\README.md %OUT_DIR% /Y
 echo F|xcopy .\LICENSE %OUT_DIR%\LICENSE_SGL.txt /Y
 echo F|xcopy src\ext\markdig\license.txt %OUT_DIR%\LICENSE_Markdig.txt /Y
 
-set ZIP_NAME=%OUT_DIR%\SpeedGuidesLive(%1).zip
+set ZIP_NAME=SpeedGuidesLiveUnofficial_%1.zip
 echo Creating %ZIP_NAME% ...
-REM actually create the zip file
+
+REM Create the zip file using PowerShell
+powershell Compress-Archive -Path "%OUT_DIR%\*" -DestinationPath "%OUT_DIR%\%ZIP_NAME%" -Force
+
+REM Delete the files after zipping
+echo Cleaning up...
+rmdir /S /Q "%OUT_DIR%\LiveSplit"
+del /Q "%OUT_DIR%\README.md"
+del /Q "%OUT_DIR%\LICENSE_SGL.txt"
+del /Q "%OUT_DIR%\LICENSE_Markdig.txt"
 
 goto end
 
